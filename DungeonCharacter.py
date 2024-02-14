ae07bd63a9ad2f11cedd70c8cde3845cf8d78f81
user <user@users-Air.lan>
2024-02-13 19:16:56 -0800

    updating Hero and DC with dictionary

--- DungeonCharacter.py
+++ DungeonCharacter.py
@@ -4,13 +4,20 @@ from abc import ABC, abstractmethod
 class DungeonCharacter:
     """ DungeonCharacter class is an abstract class used to override methods in Hero and Monster subclasses"""
     def __init__(self, name, hit_points, attack_speed, chance_to_hit, min_damage,
-                 max_damage):
-        self._name = name
-        self._hit_points = hit_points
-        self._attack_speed = attack_speed
-        self._chance_to_hit = chance_to_hit
-        self._min_damage = min_damage
-        self._max_damage = max_damage
+                 max_damage, h_potion_ct, v_potion_ct, pillar_ct):
+        # self._name = name
+        # self._hit_points = hit_points
+        # self._attack_speed = attack_speed
+        # self._chance_to_hit = chance_to_hit
+        # self._min_damage = min_damage
+        # self._max_damage = max_damage
+        # self._h_potion_ct = h_potion_ct
+        # self._v_potion_ct = v_potion_ct
+        # self._pillar = pillar_ct
+
+        self.stats = {"name": name, "hit_points": hit_points, "attack_speed": attack_speed,
+                           "chance_to_hit": chance_to_hit, "min_damage": min_damage,
+                 "max_damage": max_damage, "h_potion_ct": h_potion_ct, "v_potion_ct": v_potion_ct, "pillar_ct": pillar_ct}
 
     @abstractmethod
     def can_attack(self):
--- Hero.py
+++ Hero.py
@@ -1,4 +1,4 @@
-from DungeonCharacter import DungeonCharacter
+from DungeonCharacter_m import DungeonCharacter
 from abc import ABC, abstractmethod
 import random
 
@@ -7,10 +7,47 @@ class Hero(DungeonCharacter, ABC):
     """ Hero class inherits from DungeonCharacter parent class, and it is an abstract base class"""
 
     def __init__(self, name, hit_points, attack_speed, chance_to_hit, min_damage,
-                 max_damage, chance_to_block):
+                 max_damage, chance_to_block, h_potion_ct, v_potion_ct, pillar_ct):
         super().__init__(name, hit_points, attack_speed, chance_to_hit, min_damage,
-                         max_damage)
-        self._chance_to_block = chance_to_block
+                         max_damage, h_potion_ct, v_potion_ct, pillar_ct)
+        # self._chance_to_block = chance_to_block
+        self.stats["chance_to_block"] = chance_to_block
+
+    @abstractmethod
+    def current_name(self, name):
+        """
+        abstract method for current_name method used in child classes
+        """
+        pass
+
+    @abstractmethod
+    def hit_points(self, hit_points):
+        """ 
+        abstract method for get_hit-points method used in child classes
+        """
+        pass
+
+    @abstractmethod
+    def healing_potion_count(self, h_potion):
+        """
+        abstract method for get_hit-points method used in child classes
+        """
+        pass
+
+    @abstractmethod
+    def vision_potion_count(self, v_potion):
+        """
+        abstract method for get_hit-points method used in child classes
+        """
+        pass
+
+    @abstractmethod
+    def pillar_count(self, pillar):
+        """
+        abstract method for get_hit-points method used in child classes
+        """
+        pass
+
 
     @abstractmethod
     def can_attack(self):
@@ -42,32 +79,70 @@ class Warrior(Hero):
     """ Warrior class is the child class of Hero """
 
     def __init__(self):
-        super().__init__("Warrior", 125, 4, 0.8, 35, 60, 0.2)
+        super().__init__("Warrior", 125, 4, 0.8, 35, 60,
+                         0.2, 0, 0, 0)
+
+    def __str__(self):
+        formatted_list = ["   " +str(item) + " : " + str(values) for item, values in self.stats.items()]
+        return "\n" + "\n".join(formatted_list) + "\n"
+
+    def current_name(self, name):
+        """
+        Sets character name
+        """
+        self.stats["name"] = name
+
+
+    def hit_points(self, hit_points):
+        """
+        Sets character hitpoints
+        """
+        self.stats["hit_points"] += hit_points
+
+    def healing_potion_count(self, h_potion):
+        """
+        Sets healing potion count
+        """
+        self.stats["h_potion_ct"] += h_potion
+
+    def vision_potion_count(self, v_potion):
+        """
+        Sets and gets vision potion count
+        return: int vision potion count
+        """
+        self.stats["v_potion_ct"] += v_potion
+
+    def pillar_count(self, pillar):
+        """
+        Sets and gets pillar count
+        return: int pillar count
+        """
+        self.stats["pillar"] += pillar
 
     def calculate_damage(self, damage):
         """ This method decrements hit_points to calculate damage """
-        self._hit_points -= damage
+        self.stats["hit_points"] -= damage
 
     def get_damage(self):
         """ This method gets damage points """
-        return random.randint(self._min_damage, self._max_damage)
+        return random.randint(self.stats["min_damage"], self.stats["max_damage"])
 
     def can_attack(self):
         """ This method gives the chance or probability of chance_to_hit"""
-        return random.random() < self._chance_to_hit  # random.random() generates a float value from 0 to 1
+        return random.random() < self.stats["chance_to_hit"]  # random.random() generates a float value from 0 to 1
 
     def attack(self, opponent):
         """ This method attacks the opponent and causes damage to the opponent"""
-        print(f"{self._name} is battling against {opponent._name}")
+        print(f"{self.stats["name"]} is battling against {opponent._name}")
         if self.can_attack():  # if Warrior can attack
             damage = self.get_damage()  # gets minimum amd maximum damage points
             opponent.calculate_damage(damage)  # passes the damage points to calculate_damage method
             # and reduce the points of the opponent
-            print(f" {self._name} attacks {opponent._name} for {damage} damage points")
+            print(f" {self.stats["name"]} attacks {opponent._name} for {damage} damage points")
             return True  # return True if the attack is successful
 
         else:
-            print(f" {self._name} couldn't attack {opponent._name}  ")
+            print(f" {self.stats["name"]} couldn't attack {opponent._name}  ")
             return False  # else return False
 
     def special_skill(self, opponent):
@@ -75,41 +150,78 @@ class Warrior(Hero):
         if random.random() < 0.4:  # 40% chance for Crushing Blow
             damage = random.randint(75, 175)  # causes damage for 75 to 175 points
             opponent.calculate_damage(damage)  # passes the damage points to calculate_damage method
-            print(f"{self._name} performs a Crushing Blow for {damage} damage.")
+            print(f"{self.stats["name"]} performs a Crushing Blow for {damage} damage.")
         else:
-            print(f"{self._name} couldn't perform Crushing Blow")
+            print(f"{self.stats["name"]} couldn't perform Crushing Blow")
 
 
 class Priestess(Hero):
     """ Priestess class is the child class of Hero """
 
     def __init__(self):
-        super().__init__("Priestess", 75, 5, 0.7, 25, 45, 0.3)
+        super().__init__("Priestess", 75, 5, 0.7, 25, 45,
+                         0.3, 0, 0, 0)
+
+    def __str__(self):
+        formatted_list = ["   " +str(item) + " : " + str(values) for item, values in self.stats.items()]
+        return "\n" + "\n".join(formatted_list) + "\n"
+
+    def current_name(self, name):
+        """
+        Sets character name
+        """
+        self.stats["name"] = name
+
+    def hit_points(self, hit_points):
+        """
+        Sets character hitpoints
+        """
+        self.stats["hit_points"] += hit_points
+
+    def healing_potion_count(self, h_potion):
+        """
+        Sets healing potion count
+        """
+        self.stats["h_potion_ct"] += h_potion
+
+    def vision_potion_count(self, v_potion):
+        """
+        Sets and gets vision potion count
+        return: int vision potion count
+        """
+        self.stats["v_potion_ct"] += v_potion
+
+    def pillar_count(self, pillar):
+        """
+        Sets and gets pillar count
+        return: int pillar count
+        """
+        self.stats["pillar"] += pillar
 
     def calculate_damage(self, damage):
         """ This method decrements hit_points to calculate damage """
-        self._hit_points -= damage
+        self.stats["hit_points"] -= damage
 
     def get_damage(self):
         """ This method gets damage points """
-        return random.randint(self._min_damage, self._max_damage)
+        return random.randint(self.stats["min_damage"], self.stats["max_damage"])
 
     def can_attack(self):
         """ This method gives the chance or probability of chance_to_hit"""
-        return random.random() < self._chance_to_hit  # random.random() generates a float value from 0 to 1
+        return random.random() < self.stats["chance_to_hit"]  # random.random() generates a float value from 0 to 1
 
     def attack(self, opponent):
         """ This method attacks the opponent and causes damage to the opponent"""
-        print(f"{self._name} is battling against {opponent._name}")
-        if self.can_attack():  # if Priestess can attack
+        print(f"{self.stats["name"]} is battling against {opponent._name}")
+        if self.can_attack():  # if Warrior can attack
             damage = self.get_damage()  # gets minimum amd maximum damage points
             opponent.calculate_damage(damage)  # passes the damage points to calculate_damage method
             # and reduce the points of the opponent
-            print(f" {self._name} attacks {opponent._name} for {damage} damage points")
+            print(f" {self.stats["name"]} attacks {opponent._name} for {damage} damage points")
             return True  # return True if the attack is successful
 
         else:
-            print(f" {self._name} couldn't attack {opponent._name}  ")
+            print(f" {self.stats["name"]} couldn't attack {opponent._name}  ")
             return False  # else return False
 
     def special_skill(self, opponent):
@@ -122,31 +234,69 @@ class Thief(Hero):
     """ Thief class is the child class of Hero """
 
     def __init__(self):
-        super().__init__("Thief", 75, 6, 0.8, 20, 40, 0.4)
+        super().__init__("Thief", 75, 6, 0.8, 20, 40,
+                         0.4, 0, 0, 0)
+
+    def __str__(self):
+        formatted_list = ["   " +str(item) + " : " + str(values) for item, values in self.stats.items()]
+        return "\n" + "\n".join(formatted_list) + "\n"
+
+    def current_name(self, name):
+        """
+        Sets character name
+        """
+        self.stats["name"] = name
+
+    def hit_points(self, hit_points):
+        """
+        Sets character hitpoints
+        """
+        self.stats["hit_points"] += hit_points
+
+    def healing_potion_count(self, h_potion):
+        """
+        Sets healing potion count
+        """
+        self.stats["h_potion_ct"] += h_potion
+
+    def vision_potion_count(self, v_potion):
+        """
+        Sets and gets vision potion count
+        return: int vision potion count
+        """
+        self.stats["v_potion_ct"] += v_potion
+
+    def pillar_count(self, pillar):
+        """
+        Sets and gets pillar count
+        return: int pillar count
+        """
+        self.stats["pillar"] += pillar
 
     def calculate_damage(self, damage):
         """ This method decrements hit_points to calculate damage """
-        self._hit_points -= damage
+        self.stats["hit_points"] -= damage
 
     def get_damage(self):
         """ This method gets damage points """
-        return random.randint(self._min_damage, self._max_damage)
+        return random.randint(self.stats["min_damage"], self.stats["max_damage"])
 
     def can_attack(self):
         """ This method gives the chance or probability of chance_to_hit"""
-        return random.random() < self._chance_to_hit  # random.random() generates a float value from 0 to 1
+        return random.random() < self.stats["chance_to_hit"]  # random.random() generates a float value from 0 to 1
 
     def attack(self, opponent):
-        print(f"{self._name} is battling against {opponent._name}")
-        if self.can_attack():  # if Thief can attack
+        """ This method attacks the opponent and causes damage to the opponent"""
+        print(f"{self.stats["name"]} is battling against {opponent._name}")
+        if self.can_attack():  # if Warrior can attack
             damage = self.get_damage()  # gets minimum amd maximum damage points
             opponent.calculate_damage(damage)  # passes the damage points to calculate_damage method
             # and reduce the points of the opponent
-            print(f" {self._name} attacks {opponent._name} for {damage} damage points")
+            print(f" {self.stats["name"]} attacks {opponent._name} for {damage} damage points")
             return True  # return True if the attack is successful
 
         else:
-            print(f" {self._name} couldn't attack {opponent._name}  ")
+            print(f" {self.stats["name"]} couldn't attack {opponent._name}  ")
             return False  # else return False
 
     def special_skill(self, opponent):
@@ -155,15 +305,15 @@ class Thief(Hero):
         if chance < 0.4:  # 40% chance for Surprise Attack
             self.attack(opponent)
             self.attack(opponent)
-            print(f" {self._name} attacked twice")
+            print(f" {self.stats["name"]} attacked twice")
         elif chance < 0.8:
             self.attack(opponent)
-            print(f" {self._name} attacked once")
+            print(f" {self.stats["name"]} attacked once")
         else:
-            print(f"{self._name} couldn't attack")
+            print(f"{self.stats["name"]} couldn't attack")
 
 
 # usage
-w = Warrior()
-o = Thief()
-w.attack(o)
+# w = Warrior()
+# o = Thief()
+# w.attack(o)
