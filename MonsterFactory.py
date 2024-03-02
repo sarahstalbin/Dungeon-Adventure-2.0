ad7416ef260b52ffc87e3a2392d25454d5a97501
Sarah St. Albin <130087873+sstalbin@users.noreply.github.com>
2024-03-02 15:27:29 -0800

    Initial commit of MonsterFactory.

---
+++ MonsterFactory.py
@@ -0,0 +1,39 @@
+import Monster
+
+
+class MonsterFactory:
+    """
+    Creates and returns Monster objects for Dungeon Adventure 2.0.
+    All Monsters should be created via the MonsterFactory.
+
+    Contains one method: create_monster(), which creates a Monster object of a specified type using relevant data for
+    creating that Monster.
+
+    """
+
+    @staticmethod
+    def create_monster(monster_type, name):
+        """
+        Creates an instance of a Monster.
+        :param monster_type: the type of Monster to create.
+        :param name: the name of the Monster.
+        :return: the Monster object.
+        """
+        if monster_type.lower() == "ogre":
+            return Monster.Ogre(name)
+        elif monster_type.lower() == "gremlin":
+            return Monster.Gremlin(name)
+        elif monster_type.lower() == "skeleton":
+            return Monster.Skeleton(name)
+        elif monster_type.lower() == "troll":
+            return Monster.Troll(name)
+        elif monster_type.lower() == "chimera":
+            return Monster.Chimera(name)
+        elif monster_type.lower() == "dragon":
+            return Monster.Dragon(name)
+        else:
+            raise ValueError("Invalid monster type.")
+
+
+monster = MonsterFactory.create_monster("ogre", "Bob")
+print(monster)
