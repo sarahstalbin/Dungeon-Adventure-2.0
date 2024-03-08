45991142593804c305c45aa35d192fb19bc6c253
Minna <minnaleechae@gmail.com>
2024-03-08 10:38:51 -0800

    deleting New DC Folder

--- New DC/attack_test_s.py
+++
@@ -1,6 +0,0 @@
-from DungeonCharacterFactory import DungeonCharacterFactory
-
-d = DungeonCharacterFactory.create_character("skeleton", "Dreadbone")
-w = DungeonCharacterFactory.create_character("warrior", "Warrior")
-
-w.attack(d)
