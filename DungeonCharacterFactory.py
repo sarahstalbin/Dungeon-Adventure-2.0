39542cbd06f3084c6c9c154952efe8b86d2ff3eb
Aqueno Nirasmi <aqueno10@uw.edu>
2024-03-07 21:43:30 -0800

    updated DC as model

---
+++ DungeonCharacterFactory.py
@@ -0,0 +1,67 @@
+from Monster import Monster
+from Hero import Warrior, Priestess, Thief
+import query_database
+
+
+class DungeonCharacterFactory:
+    """
+    Creates and returns Dungeon Characters for Dungeon Adventure 2.0.
+    All Dungeon Characters should be created via the DungeonCharacterFactory.
+
+    Contains two methods: create_hero(), which creates a Hero, and create_monster(), which creates a Monster.
+    """
+
+    @staticmethod
+    def create_character(table_name, character_name):
+        conn = query_database.create_connection("dungeon_character_db.sqlite")
+        if table_name.lower() == "warrior":
+            warrior_data = query_database.select_row(conn, table_name, character_name)
+            return Warrior(**warrior_data)
+        elif table_name.lower() == "priestess":
+            priestess_data = query_database.select_row(conn, table_name, character_name)
+            return Priestess(**priestess_data)
+        elif table_name.lower() == "thief":
+            thief_data = query_database.select_row(conn, table_name, character_name)
+            return Thief(**thief_data)
+        elif table_name.lower() == "ogre":
+            monster_type = table_name
+            ogre_data = query_database.select_row(conn, table_name, character_name)
+            return Monster(monster_type, **ogre_data)
+        elif table_name.lower() == "gremlin":
+            monster_type = table_name
+            gremlin_data = query_database.select_row(conn, table_name, character_name)
+            return Monster(monster_type, **gremlin_data)
+        elif table_name.lower() == "skeleton":
+            monster_type = table_name
+            skeleton_data = query_database.select_row(conn, table_name, character_name)
+            return Monster(monster_type, **skeleton_data)
+        elif table_name.lower() == "troll":
+            monster_type = table_name
+            troll_data = query_database.select_row(conn, table_name, character_name)
+            return Monster(monster_type, **troll_data)
+        elif table_name.lower() == "chimera":
+            monster_type = table_name
+            chimera_data = query_database.select_row(conn, table_name, character_name)
+            return Monster(monster_type, **chimera_data)
+        elif table_name.lower() == "dragon":
+            monster_type = table_name
+            dragon_data = query_database.select_row(conn, table_name, character_name)
+            return Monster(monster_type, **dragon_data)
+        else:
+            raise ValueError("Invalid Dungeon Character type")
+
+
+# w = DungeonCharacterFactory.create_character("warrior", "Warrior")
+# p = DungeonCharacterFactory.create_character("priestess", "Priestess")
+# t = DungeonCharacterFactory.create_character("thief", "Thief")
+# o = DungeonCharacterFactory.create_character("ogre", "Throg")
+# g = DungeonCharacterFactory.create_character("gremlin", "Spike")
+# s = DungeonCharacterFactory.create_character("skeleton", "Rattlebones")
+# tr = DungeonCharacterFactory.create_character("troll", "Ragnok")
+# c = DungeonCharacterFactory.create_character("chimera", "Thrawn")
+# d = DungeonCharacterFactory.create_character("dragon", "Fafnir")
+#
+# print(t)
+# print(s)
+# print(c)
+# print(w)
---
+++ Monster.py
@@ -0,0 +1,132 @@
+import random
+from DungeonCharacter import DungeonCharacter
+
+
+class Monster(DungeonCharacter):
+    """ Monster class inherits from DungeonCharacter and is used to create various Monsters for Dungeon Adventure
+    2.0."""
+
+    def __init__(self, monster_type, name, hit_points, attack_speed, chance_to_hit, minimum_damage, maximum_damage,
+                 chance_to_heal, minimum_heal_points, maximum_heal_points, heal_points, has_fainted):
+        super().__init__(name, hit_points, attack_speed, chance_to_hit, minimum_damage,
+                         maximum_damage)
+        self._monster_type = monster_type
+        self._chance_to_heal = chance_to_heal
+        self._minimum_heal_points = minimum_heal_points
+        self._maximum_heal_points = maximum_heal_points
+        self._heal_points = heal_points
+        self._has_fainted = has_fainted
+        self._name = name
+
+    def __str__(self):
+        """
+        Returns a string representation of the Monster class.
+        :return: string
+        """
+        monster_info = ""
+        monster_info += f"{self.name}\n"
+        monster_info += f"Hit Points: {self.hit_points}\n"
+        monster_info += f"Heal Points: {self.heal_points}\n"
+        monster_info += f"Attack Speed: {self.attack_speed}\n"
+        monster_info += f"Chance to Hit: {self.chance_to_hit}\n"
+        monster_info += f"Minimum Damage: {self.minimum_damage}\n"
+        monster_info += f"Maximum Damage: {self.maximum_damage}\n"
+        monster_info += f"Chance to Heal: {self.chance_to_heal}\n"
+        monster_info += f"Minimum Heal Points: {self.minimum_heal_points}\n"
+        monster_info += f"Maximum Heal Points: {self.maximum_heal_points}\n"
+        monster_info += f"Heal Points: {self.heal_points}\n"
+        if self.has_fainted:
+            monster_info += f"Fainted: Yes\n"
+        else:
+            monster_info += f"Fainted: No\n"
+        return monster_info
+
+    def heal(self):
+        """ Heals the Monster based on chance to heal within range of its min/max heal points """
+        result = {"attacker": self.name, "success": False, "heal_amount": 0}
+        chance_to_heal = self.chance_to_heal
+        heal_points = self.get_random_heal_points()
+        has_fainted = self.has_fainted
+
+        if not has_fainted and chance_to_heal:
+            hit_point_count = self.hit_points
+            heal_amount = hit_point_count + heal_points
+            self.hit_points = heal_amount
+            result["success"] = True
+            result["heal_amount"] = heal_amount
+        return result
+
+    def get_random_heal_points(self):
+        """ Returns a random integer between the range of a Monster's minimum and maximum heal points """
+        min_heal_points = self.minimum_heal_points
+        max_heal_points = self.maximum_heal_points
+        return random.randint(min_heal_points, max_heal_points)
+
+    def faint(self):
+        """ Changes Monster's has_fainted boolean to True when below minimum hit point count """
+        self.has_fainted = True
+
+    @property
+    def chance_to_heal(self):
+        """ Returns chance to heal """
+        return self._chance_to_heal
+
+    @chance_to_heal.setter
+    def chance_to_heal(self, value):
+        """ Sets new chance to heal percentage """
+        if isinstance(value, float) and value > 0:
+            self._chance_to_heal = value
+        else:
+            raise ValueError("chance_to_heal value must be a float")
+
+    @property
+    def minimum_heal_points(self):
+        """ Returns current minimum heal point value """
+        return self._minimum_heal_points
+
+    @minimum_heal_points.setter
+    def minimum_heal_points(self, value):
+        """ Sets new minimum heal point value """
+        if isinstance(value, int) and value > 0:
+            self._minimum_heal_points = value
+        else:
+            raise ValueError("minimum_heal_points value must be an int")
+
+    @property
+    def maximum_heal_points(self):
+        """ Returns current maximum heal point value"""
+        return self._maximum_heal_points
+
+    @maximum_heal_points.setter
+    def maximum_heal_points(self, value):
+        """ Sets new maximum heal point value """
+        if isinstance(value, int) and value > self.minimum_heal_points:
+            self._maximum_heal_points = value
+        else:
+            raise ValueError("maximum_heal_points value must be an int")
+
+    @property
+    def heal_points(self):
+        """ Returns current heal point value """
+        return self._heal_points
+
+    @heal_points.setter
+    def heal_points(self, value):
+        """ Sets new heal point value"""
+        if isinstance(value, int) and value > 0:
+            self._heal_points = value
+        else:
+            raise ValueError("heal_point value must be an int")
+
+    @property
+    def has_fainted(self):
+        """ Returns the status of whether a Monster has fainted or not (boolean) """
+        return self._has_fainted
+
+    @has_fainted.setter
+    def has_fainted(self, boolean):
+        """ Updates the Monster's fainting status """
+        if isinstance(boolean, bool):
+            self._has_fainted = boolean
+        else:
+            raise ValueError("has_fainted must be a boolean value")
