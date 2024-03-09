79f1e5cdca57c2f738240746b31d0d00d3e436d3
Minna <minnaleechae@gmail.com>
2024-03-09 11:33:32 -0800

    V2

--- DungeonItems.py
+++ DungeonItems.py
@@ -59,67 +59,158 @@ class VisionPotion(DungeonItems):
         row = 1
         col = 1
 
+        top_t = []
+        mid_t = []
+        bottom_t = []
+        space = " "
         if current_row > 0 and current_col > 0: #upper left
-            vision_rooms.append(dungeon.get_room_str((current_row-1, current_col-1)))
+            top_t.append(str(dungeon.get_room_str((current_row-1, current_col-1)))[0:3] + space * 10)
+            mid_len = len(str(dungeon.get_room_str((current_row-1, current_col-1)))[4:-4])
+            mid_t.append(str(dungeon.get_room_str((current_row-1, current_col-1)))[4:-4] + space * (13 - mid_len))
+            bottom_t.append(str(dungeon.get_room_str((current_row-1, current_col-1)))[-3:] + (space * 10))
+
         if current_row > 0: #directly up
-            vision_rooms.append(dungeon.get_room_str((current_row - 1, current_col)))
+            top_t.append(str(dungeon.get_room_str((current_row-1, current_col)))[0:3] + space * 10)
+            mid_len = len(str(dungeon.get_room_str((current_row-1, current_col)))[4:-4])
+            mid_t.append(str(dungeon.get_room_str((current_row-1, current_col)))[4:-4] + space * (13 - mid_len))
+            bottom_t.append(str(dungeon.get_room_str((current_row-1, current_col)))[-3:] + (space * 10))
+
+            # vision_rooms.append(dungeon.get_room_str((current_row - 1, current_col)))
             row += 1
 
             if current_col +1 < dungeon_max_col: # upper right
-                vision_rooms.append(dungeon.get_room_str((current_row - 1, current_col+1)))
+                top_t.append(str(dungeon.get_room_str((current_row - 1, current_col+1)))[0:3] + space * 10)
+                mid_len = len(str(dungeon.get_room_str((current_row - 1, current_col+1)))[4:-4])
+                mid_t.append(str(dungeon.get_room_str((current_row - 1, current_col+1)))[4:-4] + space * (13 - mid_len))
+                bottom_t.append(str(dungeon.get_room_str((current_row - 1, current_col + 1)))[-3:] + (space * 10))
+
+                # vision_rooms.append(dungeon.get_room_str((current_row - 1, current_col+1)))
         if current_col > 0: #left
-            vision_rooms.append(dungeon.get_room_str((current_row, current_col - 1)))
+            top_t.append(str(dungeon.get_room_str((current_row, current_col - 1)))[0:3] + space * 10)
+            mid_len = len(str(dungeon.get_room_str((current_row, current_col -1)))[4:-4])
+            mid_t.append(str(dungeon.get_room_str((current_row, current_col - 1)))[4:-4] + space * (13 - mid_len))
+            bottom_t.append(str(dungeon.get_room_str((current_row, current_col - 1)))[-3:] + (space * 10))
+            # vision_rooms.append(dungeon.get_room_str((current_row, current_col - 1)))
             col += 1
 
-        vision_rooms.append(dungeon.get_room_str((current_row, current_col))) #current room
+        # vision_rooms.append(dungeon.get_room_str((current_row, current_col))) #current room
+        top_t.append(str(dungeon.get_room_str((current_row, current_col )))[0:3] + space * 10)
+        mid_len = len(str(dungeon.get_room_str((current_row, current_col)))[4:-4])
+        current = str(dungeon.get_room_str((current_row, current_col)))[4]
+        current += "@"
+        current += str(dungeon.get_room_str((current_row, current_col)))[-5:-4] + space * (13 - mid_len)
+        mid_t.append(current)
+        # mid_t.append(str(dungeon.get_room_str((current_row, current_col)))[4:-4] + space * (13 - mid_len))
+        bottom_t.append(str(dungeon.get_room_str((current_row, current_col)))[-3:] + (space * 10))
+
+
         if current_col + 1 < dungeon_max_col: #right
-            vision_rooms.append(dungeon.get_room_str((current_row, current_col+1)))
+            top_t.append(str(dungeon.get_room_str((current_row, current_col + 1)))[0:3] + space * 10)
+            mid_len = len(str(dungeon.get_room_str((current_row, current_col + 1)))[4:-4])
+            mid_t.append(str(dungeon.get_room_str((current_row, current_col + 1)))[4:-4] + space * (13 - mid_len))
+            bottom_t.append(str(dungeon.get_room_str((current_row , current_col + 1)))[-3:] + (space * 10))
+            # vision_rooms.append(dungeon.get_room_str((current_row, current_col+1)))
             col += 1
         if current_col > 0 and current_row + 1 < dungeon_max_row:  # below left
-            vision_rooms.append(dungeon.get_room_str((current_row - 1, current_col - 1)))
+            top_t.append(str(dungeon.get_room_str((current_row+1, current_col-1)))[0:3] + space * 10)
+            mid_len = len(str(dungeon.get_room_str((current_row+1, current_col-1)))[4:-4])
+            mid_t.append(str(dungeon.get_room_str((current_row+1, current_col-1)))[4:-4] + space * (13 - mid_len))
+            bottom_t.append(str(dungeon.get_room_str((current_row+1, current_col-1)))[-3:] + (space * 10))
+
+            # vision_rooms.append(dungeon.get_room_str((current_row - 1, current_col - 1)))
 
         if current_row +1 < dungeon_max_row: #directly below
-            vision_rooms.append(dungeon.get_room_str((current_row+1, current_col)))
+            top_t.append(str(dungeon.get_room_str((current_row+1, current_col)))[0:3] + space * 10)
+            mid_len = len(str(dungeon.get_room_str((current_row+1, current_col)))[4:-4])
+            mid_t.append(str(dungeon.get_room_str((current_row+1, current_col)))[4:-4] + space * (13 - mid_len))
+            bottom_t.append(str(dungeon.get_room_str((current_row+1 , current_col)))[-3:] + (space * 10))
+            # vision_rooms.append(dungeon.get_room_str((current_row+1, current_col)))
             row += 1
         if current_col + 1 < dungeon_max_col and current_row + 1 < dungeon_max_row:  # below right
-            vision_rooms.append(dungeon.get_room_str((current_row + 1, current_col + 1)))
+            top_t.append(str(dungeon.get_room_str((current_row+1, current_col + 1)))[0:3] + space * 10)
+            mid_len = len(str(dungeon.get_room_str((current_row+1, current_col + 1)))[4:-4])
+            mid_t.append(str(dungeon.get_room_str((current_row+1, current_col + 1)))[4:-4] + space * (13 - mid_len))
+            bottom_t.append(str(dungeon.get_room_str((current_row+1 , current_col + 1)))[-3:] + (space * 10))
+            # vision_rooms.append(dungeon.get_room_str((current_row + 1, current_col + 1)))
 
-        top = []
-        space = " "
 
-        for rooms in vision_rooms:
-            # if str(rooms) != "":
-            top.append(str(rooms)[0:3] + space * 10)
-        # mid
-        mid = []
-        for rooms in vision_rooms:
-            mid_len = len(str(rooms)[4:-4])
-            # if str(rooms) != "":
-                # if len(str(rooms)) == 10:
-            mid.append(str(rooms)[4:-4] + space * (13 - mid_len))
-                # else:
-                    # mid.append(str(rooms)[4:7] + "    ")
-
-        # bottom
-        bottom = []
-        for rooms in vision_rooms:
-            # if str(rooms) != "":
-                # if len(str(rooms)) == 10:
-            bottom.append(str(rooms)[-3:] + (space * 10))
-                # else:
-                    # bottom.append(str(rooms)[8:11] + "    ")
-
-        # Printing View
         for i in range(0, row):
             for room in range(i * col, (i + 1) * col):
-                print(top[room], end="")
+                print(top_t[room], end="")
             print(end="\n")
             for room in range(i * col, (i + 1) * col):
-                print(mid[room], end="")
+                print(mid_t[room], end="")
             print(end="\n")
             for room in range(i * col, (i + 1) * col):
-                print(bottom[room], end="")
+                print(bottom_t[room], end="")
             print("\n")
+
+
+
+        #
+        # if current_row > 0 and current_col > 0: #upper left
+        #     vision_rooms.append(dungeon.get_room_str((current_row-1, current_col-1)))
+        # if current_row > 0: #directly up
+        #     vision_rooms.append(dungeon.get_room_str((current_row - 1, current_col)))
+        #     row += 1
+        #
+        #     if current_col +1 < dungeon_max_col: # upper right
+        #         vision_rooms.append(dungeon.get_room_str((current_row - 1, current_col+1)))
+        # if current_col > 0: #left
+        #     vision_rooms.append(dungeon.get_room_str((current_row, current_col - 1)))
+        #     col += 1
+        # print(dungeon.get_room_str((current_row, current_col)))
+        # vision_rooms.append(dungeon.get_room_str((current_row, current_col))) #current room
+        # if current_col + 1 < dungeon_max_col: #right
+        #     vision_rooms.append(dungeon.get_room_str((current_row, current_col+1)))
+        #     col += 1
+        # if current_col > 0 and current_row + 1 < dungeon_max_row:  # below left
+        #     print("I am here")
+        #     print(dungeon.get_room_str((current_row - 1, current_col - 1)))
+        #     vision_rooms.append(dungeon.get_room_str((current_row - 1, current_col - 1)))
+        #
+        # if current_row +1 < dungeon_max_row: #directly below
+        #     vision_rooms.append(dungeon.get_room_str((current_row+1, current_col)))
+        #     row += 1
+        # if current_col + 1 < dungeon_max_col and current_row + 1 < dungeon_max_row:  # below right
+        #     vision_rooms.append(dungeon.get_room_str((current_row + 1, current_col + 1)))
+
+        # top = []
+        # space = " "
+        #
+        # for rooms in vision_rooms:
+        #     # if str(rooms) != "":
+        #     top.append(str(rooms)[0:3] + space * 10)
+        # # mid
+        # mid = []
+        # for rooms in vision_rooms:
+        #     mid_len = len(str(rooms)[4:-4])
+        #     # if str(rooms) != "":
+        #         # if len(str(rooms)) == 10:
+        #     mid.append(str(rooms)[4:-4] + space * (13 - mid_len))
+        #         # else:
+        #             # mid.append(str(rooms)[4:7] + "    ")
+        #
+        # # bottom
+        # bottom = []
+        # for rooms in vision_rooms:
+        #     # if str(rooms) != "":
+        #         # if len(str(rooms)) == 10:
+        #     bottom.append(str(rooms)[-3:] + (space * 10))
+        #         # else:
+        #             # bottom.append(str(rooms)[8:11] + "    ")
+        #
+        # # Printing View
+        # for i in range(0, row):
+        #     for room in range(i * col, (i + 1) * col):
+        #         print(top[room], end="")
+        #     print(end="\n")
+        #     for room in range(i * col, (i + 1) * col):
+        #         print(mid[room], end="")
+        #     print(end="\n")
+        #     for room in range(i * col, (i + 1) * col):
+        #         print(bottom[room], end="")
+        #     print("\n")
 
 
     # def current_room(self, room):
