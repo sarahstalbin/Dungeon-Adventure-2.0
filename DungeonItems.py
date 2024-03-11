b136a70be01be5a8a050bdf82ede8eb4a9d72890
Minna <minnaleechae@gmail.com>
2024-03-11 16:08:04 -0700

    V3

--- DungeonItems.py
+++ DungeonItems.py
@@ -133,104 +133,19 @@ class VisionPotion(DungeonItems):
             bottom_t.append(str(dungeon.get_room_str((current_row+1 , current_col + 1)))[-3:] + (space * 10))
             # vision_rooms.append(dungeon.get_room_str((current_row + 1, current_col + 1)))
 
-
+        vision_str = ""
         for i in range(0, row):
             for room in range(i * col, (i + 1) * col):
-                print(top_t[room], end="")
-            print(end="\n")
+                vision_str += top_t[room]
+            vision_str += '\n'
             for room in range(i * col, (i + 1) * col):
-                print(mid_t[room], end="")
-            print(end="\n")
+                vision_str += mid_t[room]
+            vision_str += '\n'
             for room in range(i * col, (i + 1) * col):
-                print(bottom_t[room], end="")
-            print("\n")
+                vision_str += bottom_t[room]
+            vision_str += '\n'
+        return vision_str
 
-
-
-        #
-        # if current_row > 0 and current_col > 0: #upper left
-        #     vision_rooms.append(dungeon.get_room_str((current_row-1, current_col-1)))
-        # if current_row > 0: #directly up
-        #     vision_rooms.append(dungeon.get_room_str((current_row - 1, current_col)))
-        #     row += 1
-        #
-        #     if current_col +1 < dungeon_max_col: # upper right
-        #         vision_rooms.append(dungeon.get_room_str((current_row - 1, current_col+1)))
-        # if current_col > 0: #left
-        #     vision_rooms.append(dungeon.get_room_str((current_row, current_col - 1)))
-        #     col += 1
-        # print(dungeon.get_room_str((current_row, current_col)))
-        # vision_rooms.append(dungeon.get_room_str((current_row, current_col))) #current room
-        # if current_col + 1 < dungeon_max_col: #right
-        #     vision_rooms.append(dungeon.get_room_str((current_row, current_col+1)))
-        #     col += 1
-        # if current_col > 0 and current_row + 1 < dungeon_max_row:  # below left
-        #     print("I am here")
-        #     print(dungeon.get_room_str((current_row - 1, current_col - 1)))
-        #     vision_rooms.append(dungeon.get_room_str((current_row - 1, current_col - 1)))
-        #
-        # if current_row +1 < dungeon_max_row: #directly below
-        #     vision_rooms.append(dungeon.get_room_str((current_row+1, current_col)))
-        #     row += 1
-        # if current_col + 1 < dungeon_max_col and current_row + 1 < dungeon_max_row:  # below right
-        #     vision_rooms.append(dungeon.get_room_str((current_row + 1, current_col + 1)))
-
-        # top = []
-        # space = " "
-        #
-        # for rooms in vision_rooms:
-        #     # if str(rooms) != "":
-        #     top.append(str(rooms)[0:3] + space * 10)
-        # # mid
-        # mid = []
-        # for rooms in vision_rooms:
-        #     mid_len = len(str(rooms)[4:-4])
-        #     # if str(rooms) != "":
-        #         # if len(str(rooms)) == 10:
-        #     mid.append(str(rooms)[4:-4] + space * (13 - mid_len))
-        #         # else:
-        #             # mid.append(str(rooms)[4:7] + "    ")
-        #
-        # # bottom
-        # bottom = []
-        # for rooms in vision_rooms:
-        #     # if str(rooms) != "":
-        #         # if len(str(rooms)) == 10:
-        #     bottom.append(str(rooms)[-3:] + (space * 10))
-        #         # else:
-        #             # bottom.append(str(rooms)[8:11] + "    ")
-        #
-        # # Printing View
-        # for i in range(0, row):
-        #     for room in range(i * col, (i + 1) * col):
-        #         print(top[room], end="")
-        #     print(end="\n")
-        #     for room in range(i * col, (i + 1) * col):
-        #         print(mid[room], end="")
-        #     print(end="\n")
-        #     for room in range(i * col, (i + 1) * col):
-        #         print(bottom[room], end="")
-        #     print("\n")
-
-
-    # def current_room(self, room):
-    #     """
-    #     Sets player's current coordinates as current room. Used in vision potion
-    #     """
-    #     # if isinstance(room, Room):
-    #     room.multiple_items = False
-    #     room.healing_potion = False
-    #     room.vision_potion = False
-    #     room.pit = False
-    #     room.entrance = False
-    #     room.empty_room = False
-    #     room.abstraction_pillar = False
-    #     room.polymorphism_pillar = False
-    #     room.inheritance_pillar = False
-    #     room.encapsulation_pillar = False
-    #     room.current_room = True
-    #     # else:
-        #     raise ValueError("Must submit a Room object")
 
 class Pit(DungeonItems):
     def __init__(self, minimum, maximum):
