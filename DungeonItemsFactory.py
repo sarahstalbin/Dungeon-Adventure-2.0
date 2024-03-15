d47ddff997913dea2162998a18476f27a5dbc4be
Sarah St. Albin <130087873+sstalbin@users.noreply.github.com>
2024-03-15 11:18:43 -0700

    Update documentation.

--- DungeonItemsFactory.py
+++ DungeonItemsFactory.py
@@ -1,20 +1,19 @@
 """
 Name: Aqueno Nirasmi, Minna Chae, Sarah St. Albin
-TCSS 501 and 502
-Dungeon Adventure
+TCSS 504
+Dungeon Adventure 2.0
 """
 
-from DungeonItems import HealingPotion, VisionPotion, Pit
-from random import randint
-import random
+from DungeonItems import HealingPotion, VisionPotion, Pit, MultiItem
+
 
 class DungeonItemsFactory:
     """
     Creates and returns Item objects for the Dungeon Adventure Game.
     All Items should be created via the DungeonItemsFactory.
 
-    Contains two methods: create_item(), which creates an Item object of a specified type using relevant data for
-    creating that Item, and create_multiple_items(), which creates multiple Item objects.
+    Contains one method, create_item(), which creates an Item object of a specified type using relevant data for
+    creating that Item.
 
     """
 
@@ -22,9 +21,9 @@ class DungeonItemsFactory:
     def create_item(item_type, *args):
         """
         Static method that returns Items for the Dungeon.
-        :param item_type (Str): The type of Item to be instantiated.
+        :param item_type: The type of Item to be instantiated.
         :param args: Variable arguments based on the Item to be instantiated.
-        :return: an Item object for the Dungeon Adventure Game.
+        :return: an Item object for Dungeon Adventure 2.0.
         """
         if item_type == "V":
             return VisionPotion()
@@ -32,29 +31,8 @@ class DungeonItemsFactory:
             return HealingPotion(*args)
         elif item_type == "X":
             return Pit(*args)
+        elif item_type == "M":
+            return MultiItem()
         else:
             raise ValueError("Invalid item type")
-
-    @staticmethod
-    def create_multiple_items(*args):
-        """
-        Creates multiple Item objects using relevant data for instantiating that object.
-        :param args: necessary data for instantiating an object. Some objects require no parameters.
-        :return: an array of Item objects.
-        """
-        items = ["V", "H", "X"]
-        results = random.sample(items, randint(2, 3))
-        created_items = []
-
-        for value in results:
-            if value == "V":
-                created_items.append(VisionPotion())
-            elif value == "H":
-                created_items.append(HealingPotion(*args))
-            elif value == "X":
-                created_items.append(Pit(*args))
-            else:
-                raise ValueError("Invalid item type")
-        return created_items
-
-
+        
