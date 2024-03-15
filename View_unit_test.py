ee74d635df9933b5614a309f5d044c9a6237097b
Aqueno Nirasmi <aqueno10@uw.edu>
2024-03-15 12:12:13 -0700

    updated with monster printing

--- View_unit_test.py
+++ View_unit_test.py
@@ -1,10 +1,10 @@
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 from io import StringIO
 from View import View
 from Hero import Hero, Warrior, Priestess, Thief
 from Monster import Monster
-from Dungeon_Model import Dungeon
+from Dungeon import Dungeon
 
 
 class TestView(unittest.TestCase):
@@ -23,7 +23,7 @@ class TestView(unittest.TestCase):
 
     def test_introduction(self):
         """ Test for introduction method"""
-        expected_output = "\nWelcome to the Dungeon Adventure Game, where you may traverse a dangerous maze in hopes \nof finding the 4 pillars of Object Oriented Programing (OOP) - Abstraction, Encapsulation, \nInheritance, and Polymorphism. Within the dungeon's maze you will find surprises, such \nas healing potions, vision potions, or pits. Items will be stored and healing potions \ncan be used to increase your HP, or health points. The Vision potion allows you to see \nthrough the walls of surrounding rooms. Falling into a pit will lower your HP. Be careful \nnot to die and lose the game. Survive and find all pillars of OOP to win. \n \nTo travel, press keys w for Up, s for Down, a for Left, and d for Right. \nPress \"m\" to view menu options for legend key.\n\n"
+        expected_output = "\nWelcome to the Dungeon Adventure Game, where you may traverse a dangerous maze in hopes of finding\nthe 4 pillars of Object Oriented Programing (OOP) - Abstraction, Encapsulation, Inheritance, and\nPolymorphism. Within the dungeon's maze you will find surprises, such as healing potions, vision\npotions, monsters, or pits. Items will be stored and healing potions can be used to increase\nyour HP, or health points. The Vision potion allows you to see through the walls of surrounding\n rooms. Falling into a pit will lower your HP. You will also battle powerful monsters.Be careful\nnot to die and lose the game. Survive, find all pillars of OOP and make it to the exit to win. \n \nTo travel, press keys w for Up, s for Down, a for Left, and d for Right. \nPress \"m\" to view menu options for legend key.\n\n"
         self.assert_stdout(expected_output, self.view.introduction)
 
     def test_row_col(self):
@@ -135,7 +135,6 @@ class TestView(unittest.TestCase):
             self.view.special_attack_results(result, hero, "Warrior", monster)
             mock_print.assert_called_once_with(expected_output)
 
-
     def assert_stdout_dungeon(self, expected_output, function_to_test, dungeon):
         """ assert stdout for dungeon """
         with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
@@ -150,6 +149,35 @@ class TestView(unittest.TestCase):
         expected_output = '^^^  ^^^  ^^^  \n^^^  ^^^  ^^^  \n^^^  ^^^  ^^^  \n\n^^^  ^^^  ^^^  \n^^^  ^^^  ^^^  \n^^^  ^^^  ^^^  \n\n^^^  ^^^  ^^^  \n^^^  ^^^  ^^^  \n^^^  ^^^  ^^^  \n\n'
 
         self.assert_stdout_dungeon(expected_output, View.print_play_dungeon, dungeon)
+
+    def test_attack_mode(self):
+        """ Test attack mode method from View and compare the output"""
+        troll_monster = Monster(monster_type="Troll", name="Ragnok", hit_points=100, attack_speed=0.5,
+                                chance_to_hit=0.3, minimum_damage=40, maximum_damage=80, chance_to_heal=0.6,
+                                minimum_heal_points=50, maximum_heal_points=100, heal_points=30, has_fainted=False)
+
+        # Expected output
+        expected_output = """_______  
+ ((      ))    _______
+ ((      ))   /_     _\\
+ ((      ))   \\|0   0|/
+  __(())__    (_  ^  _)
+   ( () )   /`\\ |\"\"\"|/`\\
+     {}____/  / _____/  /
+     () ____/\\    =(   /\\
+     {}    /  \\_/\\=/\\_/  \\
+     {}    /  \\_/\\=/\\_/  \\"""
+
+        with patch('builtins.input', return_value=''):  # Mocking input function
+            with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+
+                View.attack_mode(troll_monster)
+
+                # Capture the actual output
+                actual_output = mock_stdout.getvalue().strip()
+
+                # Assert that the actual output matches the expected output
+                self.assertEqual(actual_output, expected_output)
 
     def test_end(self):
         """ Tests end printing"""
