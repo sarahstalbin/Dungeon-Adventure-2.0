1e592b64030a5a642c538c4a11c813d04b562b76
Sarah St. Albin <130087873+sstalbin@users.noreply.github.com>
2024-03-15 10:57:44 -0700

    Finish documentation adjustment.

--- dungeon_items_factory_unit_tests.py
+++ dungeon_items_factory_unit_tests.py
@@ -10,17 +10,26 @@ from DungeonItems import VisionPotion, HealingPotion, Pit
 
 class TestDungeonItemsFactory(unittest.TestCase):
     def test_create_vision_potion(self):
-        """ Verifies that a Vision Potion is created as expected """
+        """
+        Verifies that a Vision Potion is created as expected
+        :return: None
+        """
         vision_potion = DungeonItemsFactory.create_item("V")
         self.assertIsInstance(vision_potion, VisionPotion)
 
     def test_create_healing_potion(self):
-        """ Verifies that a Healing Potion is created as expected """
+        """
+        Verifies that a Healing Potion is created as expected
+        :return: None
+        """
         healing_potion = DungeonItemsFactory.create_item("H", 1, 10)
         self.assertIsInstance(healing_potion, HealingPotion)
 
     def test_create_pit(self):
-        """ Verifies that a Pit is created as expected """
+        """
+        Verifies that a Pit is created as expected
+        :return: None
+        """
         pit = DungeonItemsFactory.create_item("X", 1, 10)
         self.assertIsInstance(pit, Pit)
 
