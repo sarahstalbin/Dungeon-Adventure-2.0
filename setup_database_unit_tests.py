ec621100a3bd36a403bfe2fb3feb4e984af4d103
Sarah St. Albin <130087873+sstalbin@users.noreply.github.com>
2024-03-15 11:03:53 -0700

    Finish improved documentation.

--- setup_database_unit_tests.py
+++ setup_database_unit_tests.py
@@ -13,17 +13,26 @@ from setup_database import create_connection, create_table, main
 class TestDatabaseSetup(unittest.TestCase):
 
     def setUp(self):
-        """ Sets up in-memory database for testing """
+        """
+        Sets up in-memory database for testing
+        :return: None
+        """
         self.db_file = ":memory:"  # Use in-memory database for testing
 
     def test_create_connection(self):
-        """ Verifies that a connection is successfully created """
+        """
+        Verifies that a connection is successfully created
+        :return: None
+        """
         conn = create_connection(self.db_file)
         self.assertIsInstance(conn, sqlite3.Connection)
         conn.close()
 
     def test_create_table(self):
-        """ Verifies that a table is successfully created as expected """
+        """
+        Verifies that a table is successfully created as expected
+        :return: None
+        """
         create_table_sql = """
             CREATE TABLE IF NOT EXISTS test_table (
                 id INTEGER PRIMARY KEY,
@@ -39,11 +48,16 @@ class TestDatabaseSetup(unittest.TestCase):
         cursor.close()
         conn.close()
 
-    @patch('builtins.print')  # Mock print function
+    @patch('builtins.print')
     def test_main(self, mock_print):
-        """ Test that Main works as expected """
+        """
+        Test that Main works as expected
+        :param mock_print: a MagicMock object of the print function
+        :return: None
+        """
         main()
-        mock_print.assert_not_called()  # Assert print wasn't called, so tables & database successfully created
+        # Assert print wasn't called because it means tables & database are successfully created
+        mock_print.assert_not_called()
 
 
 if __name__ == '__main__':
